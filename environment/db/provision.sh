6569586962ce5f1727c4653bd4c808ca5ad373aa
Temi Anibaba <temianibaba@gmail.com>
2024-05-13 13:05:46 +0100

    execute db

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # be careful of these keys, they will go out of date
-curl DEBIAN_FRONTEND=noninteractive -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
-
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
 echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
@@ -17,7 +12,9 @@ sudo apt-get upgrade -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
 # remove the default .conf and replace with our configuration
-sudo sed -i -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/"0.0.0.0"/ /etc/mongod.conf
+sudo rm /etc/mongod.conf
+cd /home/ubuntu/environment/db/
+sudo mv mongod.conf /etc/
 
 # if mongo is is set up correctly these will be successful
 sudo systemctl enable mongod
